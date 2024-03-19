db45bd0337d14cee526cb57c4a04da2348a3065f
CarlosE-ML <carlosmonteroleon@gmail.com>
2024-03-19 13:02:47 -0500

    Función del formulario

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ if(textoTitulo=="Carlos1"){
 }
 
 //FUNCIONES
-let nombre ="Ani";
+//let nombre ="Ani";
 let ciudad ="Lima";
 let gusto ="Play";
 
@@ -40,3 +40,35 @@ menu_responsive.onclick = function () {
     navBar = document.querySelector(".navbar");
     navBar.classList.toggle("active");
 };
+
+//FUNCIONES DEL FORMULARIO
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
